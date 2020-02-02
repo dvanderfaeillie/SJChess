e1bf289673b1c3d7739c6b0e69c452d9c7975f63
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2020-02-02 23:40:49 +0100

    Added database initialization

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ const path = require('path')
 
 process.env.NODE_ENV = 'production'
 
-app.on("ready", () => {
+app.on('ready', () => {
+	// Initializing the database file
+	const fs = require('fs')
+	const file = './src/test.db'
+	try {
+	  if (!fs.existsSync(file)) {
+			const sqlite3 = require('sqlite3')
+			var db = new sqlite3.Database(file)
+			db.serialize(function() {
+				db.run('CREATE TABLE partijen( id INTEGER PRIMARY KEY AUTOINCREMENT, whitePlayerId INTEGER, blackPlayerId INTEGER, result INTERGER DEFAULT 0, date DATE, ts DATETIME DEFAULT CURRENT_TIMESTAMP)')
+				db.run('CREATE TABLE players( id INTEGER PRIMARY KEY AUTOINCREMENT, surname VARCHAR(128), name VARCHAR(128), sex CHAR, tournamentId INT)')
+				db.run('CREATE TABLE tournaments( id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR (128), date DATE, ts DATETIME DEFAULT CURRENT_TIMESTAMP , active BOOLEAN DEFAULT 1)')
+			})
+	  }
+	} catch(err) {
+	  console.error(err)
+	}
+
+
 	let mainWindow = new BrowserWindow({
     frame: true,
     height: 600,
@@ -16,17 +34,10 @@ app.on("ready", () => {
 	mainWindow.once('ready-to-show', () => { mainWindow.show() })
 
 	if(process.env.NODE_ENV === 'production'){
-		mainWindow.setMenu(null);
+		mainWindow.setMenu(null)
 	} else {
 		mainWindow.openDevTools()
 	}
-
-	//ipcMain.on("mainWindowLoaded", function () {
-	//	let result = knex.select("voornaam").from("spelers")
-	//	result.then(function(rows){
-	//		mainWindow.webContents.send("resultSent", rows);
-	//	})
-	//});
 });
 
 
